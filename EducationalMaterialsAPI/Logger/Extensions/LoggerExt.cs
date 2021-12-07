4522727191d64645548322a982c1fe756466a4b6
mpilchPL <36770026+mpilchPL@users.noreply.github.com>
2021-12-07 12:16:54 +0100

    Add extension method for logger which displays info abt current user and action

---
+++ EducationalMaterialsAPI/Logger/Extensions/LoggerExt.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Claims;
+
+namespace EducationalMaterialsAPI.Logger.Extensions
+{
+    public static class LoggerExt
+    {
+        public static void LogInfo<T>(this ILogger<T> logger, ClaimsPrincipal? user, string action)
+        {
+            if(user == null)
+            {
+                logger.LogInformation($"[unauthorized] Action: {action}");
+            }
+            else
+            {
+                logger.LogInformation($"[UserName: {user.Identity.Name} | Admin: {user.IsInRole("Admin")}] Action: {action}");
+            }
+        }
+    }
+}
