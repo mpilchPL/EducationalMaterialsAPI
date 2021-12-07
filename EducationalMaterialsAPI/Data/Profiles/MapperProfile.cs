f73bb3dc3ce9c0cbe455092bc8ec5c36582a99ff
mpilchPL <36770026+mpilchPL@users.noreply.github.com>
2021-12-07 09:57:50 +0100

    Add profile maps for automapper

---
+++ EducationalMaterialsAPI/Data/Profiles/MapperProfile.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using EducationalMaterialsAPI.Data.DTOs.AuthorDtos;
+using EducationalMaterialsAPI.Data.DTOs.EduMaterialDtos;
+using EducationalMaterialsAPI.Data.DTOs.EduMaterialTypeDtos;
+using EducationalMaterialsAPI.Data.DTOs.ReviewDtos;
+using EducationalMaterialsAPI.Model.Entities;
+
+namespace EducationalMaterialsAPI.Data.Profiles
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            #region EduMaterial maps
+            CreateMap<EduMaterial, EduMaterialReadDto>();
+            CreateMap<EduMaterialCreateDto, EduMaterial>();
+            CreateMap<EduMaterialUpdateDto, EduMaterial>();
+            CreateMap<EduMaterial, EduMaterialUpdateDto>();
+            #endregion
+
+            #region EduMaterialTypes maps
+            CreateMap<EduMaterialType, EduMaterialTypeReadDto>();
+            CreateMap<EduMaterialTypeCreateDto, EduMaterialType>();
+            CreateMap<EduMaterialTypeUpdateDto, EduMaterialType>();
+            CreateMap<EduMaterialType, EduMaterialTypeUpdateDto>();
+            #endregion
+
+            #region Authors maps
+            CreateMap<Author, AuthorReadDto>();
+            CreateMap<AuthorCreateDto, Author>();
+            CreateMap<AuthorUpdateDto, Author>();
+            CreateMap<Author, AuthorUpdateDto>();
+            #endregion
+
+            #region Reviews maps
+            CreateMap<Review, ReviewReadDto>();
+            CreateMap<ReviewCreateDto, Review>();
+            CreateMap<ReviewUpdateDto, Review>();
+            CreateMap<Review, ReviewUpdateDto>();
+            #endregion
+        }
+    }
+}
