5869a5dbb9c92cbc3b4035c98f0fa39b294be60f
mpilchPL <36770026+mpilchPL@users.noreply.github.com>
2021-12-07 09:58:29 +0100

    Add generic repository with basic crud methods

---
+++ EducationalMaterialsAPI/Data/Repository/IRepo.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace EducationalMaterialsAPI.Data.Repository
+{
+    public interface IRepo<Type> where Type : class
+    {
+        public Task<ICollection<Type>> GetAll();
+        public Task<ICollection<Type>> GetAll(Expression<Func<Type, bool>> predicate);
+        public Task<Type> Get(int id);
+        public Task Delete(Type type);
+        public Task Add(Type type);
+        public Task Update(Type type);
+        public Task SaveChanges();
+    }
+}
---
+++ EducationalMaterialsAPI/Data/Repository/Repo.cs
@@ -0,0 +1,42 @@
+﻿using EducationalMaterialsAPI.Data.DAL;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace EducationalMaterialsAPI.Data.Repository
+{
+    public class Repo<Type> : IRepo<Type> where Type : class
+    {
+        private readonly EduMaterialsContext _context;
+        public Repo(EduMaterialsContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ICollection<Type>> GetAll() => await _context.Set<Type>().ToListAsync();
+
+        public async Task<ICollection<Type>> GetAll(Expression<Func<Type, bool>> predicate)
+        {
+            return await _context.Set<Type>().Where(predicate).ToListAsync();
+        }
+
+        public async Task<Type> Get(int id) => await _context.Set<Type>().FindAsync(id);
+        public async Task Add(Type type)
+        {
+            await _context.Set<Type>().AddAsync(type);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(Type type)
+        {
+            _context.Set<Type>().Remove(type);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Update(Type type)
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task SaveChanges() => await _context.SaveChangesAsync();
+    }
+}
