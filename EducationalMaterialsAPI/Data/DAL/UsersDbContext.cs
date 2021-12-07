72cd65554335c6b3f71052d6ce86f2cb679b3bfb
mpilchPL <36770026+mpilchPL@users.noreply.github.com>
2021-12-07 15:54:32 +0100

    Create separate db for users

---
+++ EducationalMaterialsAPI/Data/DAL/UsersDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EducationalMaterialsAPI.Model.User;
+using Microsoft.EntityFrameworkCore;
+
+namespace EducationalMaterialsAPI.Data.DAL
+{
+    public class UsersDbContext : DbContext
+    {
+        public UsersDbContext(DbContextOptions<UsersDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Seed();
+        }
+    }
+}
