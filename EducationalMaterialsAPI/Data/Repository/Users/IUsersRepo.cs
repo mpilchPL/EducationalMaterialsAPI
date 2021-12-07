a50415482ffe42a6344d62d6fd6197d3eb80ef91
mpilchPL <36770026+mpilchPL@users.noreply.github.com>
2021-12-07 15:55:20 +0100

    Add repository for user

---
+++ EducationalMaterialsAPI/Data/Repository/Users/IUsersRepo.cs
@@ -0,0 +1,12 @@
+﻿using EducationalMaterialsAPI.Model.User;
+
+namespace EducationalMaterialsAPI.Data.Repository.Users
+{
+    public interface IUsersRepo
+    {
+        public Task Add(User user);
+        public Task<User> Get(int id);
+        public Task<User> Get(string username, string password);
+        public Task<ICollection<User>> GetAll();
+    }
+}
---
+++ EducationalMaterialsAPI/Data/Repository/Users/UsersRepo.cs
@@ -0,0 +1,33 @@
+﻿using EducationalMaterialsAPI.Data.DAL;
+using EducationalMaterialsAPI.Model.User;
+using Microsoft.EntityFrameworkCore;
+
+namespace EducationalMaterialsAPI.Data.Repository.Users
+{
+    public class UsersRepo : IUsersRepo
+    {
+        private readonly UsersDbContext _context;
+        public UsersRepo(UsersDbContext context)
+        {
+            _context = context;
+        }
+        public async Task Add(User user)
+        {
+            if(user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<User> Get(int id) => await _context.Users.FindAsync(id);
+
+        public async Task<User> Get(string username, string password) 
+        {
+            return await _context.Users.Where(u => u.Name == username && u.Password == password).FirstOrDefaultAsync();
+        }
+
+        public async Task<ICollection<User>> GetAll() => await _context.Users.ToListAsync();
+    }
+}
