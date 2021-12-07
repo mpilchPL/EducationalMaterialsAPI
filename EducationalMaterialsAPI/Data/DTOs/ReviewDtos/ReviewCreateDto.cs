d5d5326e19f5850348e748fd6837b0bdd4885f52
mpilchPL <36770026+mpilchPL@users.noreply.github.com>
2021-12-07 12:38:11 +0100

    Add DataAnnotation validation for ReviewDtos

--- EducationalMaterialsAPI/Data/DTOs/ReviewDtos/ReviewCreateDto.cs
+++ EducationalMaterialsAPI/Data/DTOs/ReviewDtos/ReviewCreateDto.cs
@@ -1,9 +1,16 @@
-﻿namespace EducationalMaterialsAPI.Data.DTOs.ReviewDtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EducationalMaterialsAPI.Data.DTOs.ReviewDtos
 {
     public class ReviewCreateDto
     {
+        [Required]
+        [StringLength(255, MinimumLength = 1)]
         public string ReviewDescription { get; set; }
+        [Required]
+        [Range(1, 10)]
         public int ReviewPoints { get; set; }
+        [Required]
         public int EduMaterialId { get; set; }
     }
 }
