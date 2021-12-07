c57357d1e6dbf667bc81335de1b6da964a0050d5
mpilchPL <36770026+mpilchPL@users.noreply.github.com>
2021-12-07 12:37:08 +0100

    Add DataAnnotation validation for AuthorDto

--- EducationalMaterialsAPI/Data/DTOs/AuthorDtos/AuthorCreateDto.cs
+++ EducationalMaterialsAPI/Data/DTOs/AuthorDtos/AuthorCreateDto.cs
@@ -1,8 +1,14 @@
-﻿namespace EducationalMaterialsAPI.Data.DTOs.AuthorDtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EducationalMaterialsAPI.Data.DTOs.AuthorDtos
 {
     public class AuthorCreateDto
     {
+        [Required]
+        [StringLength(255, MinimumLength = 1)]
         public string Name { get; set; }
+        [Required]
+        [StringLength(255, MinimumLength = 1)]
         public string Description { get; set; }
     }
 }
