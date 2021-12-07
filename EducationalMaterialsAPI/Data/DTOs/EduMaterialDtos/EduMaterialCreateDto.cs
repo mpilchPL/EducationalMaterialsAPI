3dd0f6c0f7a041b7ac7109180590ee037a2487e1
mpilchPL <36770026+mpilchPL@users.noreply.github.com>
2021-12-07 12:37:36 +0100

    Add DataAnnotation validation for EduMaterialsDtos

--- EducationalMaterialsAPI/Data/DTOs/EduMaterialDtos/EduMaterialCreateDto.cs
+++ EducationalMaterialsAPI/Data/DTOs/EduMaterialDtos/EduMaterialCreateDto.cs
@@ -4,10 +4,18 @@ namespace EducationalMaterialsAPI.Data.DTOs.EduMaterialDtos
 {
     public class EduMaterialCreateDto
     {
+        [Required]
         public int AuthorId { get; set; }
+        [Required]
+        [StringLength(255, MinimumLength = 1)]
         public string Title { get; set; }
+        [Required]
+        [StringLength(255, MinimumLength = 1)]
         public string Description { get; set; }
+        [Required]
+        [StringLength(255, MinimumLength = 1)]
         public string Location { get; set; }
+        [Required]
         public int EduMaterialTypeId { get; set; }
         [DataType(DataType.Date)]
         public DateTime PublishDate { get; set; }
